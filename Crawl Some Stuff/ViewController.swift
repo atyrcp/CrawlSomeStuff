6668430a55beb34e968c7530d8007628fa5b4215
atyrcp <atyrcp@gmail.com>
2019-06-29 14:19:37 +0800

    scrape from instagram account of NASA, but still not parse yet

--- Crawl Some Stuff/ViewController.swift
+++ Crawl Some Stuff/ViewController.swift
@@ -9,9 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    func scrapeInstagramData(from url: String) {
+        let baseURL = "https://www.instagram.com/"
+        guard let url = URL(string: baseURL + url) else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {return}
+            guard let htmlString = String(data: data, encoding: .utf8) else {return}
+            
+            print(htmlString)
+            
+        }
+        
+        task.resume()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrapeInstagramData(from: "nasa")
         // Do any additional setup after loading the view.
     }
 
