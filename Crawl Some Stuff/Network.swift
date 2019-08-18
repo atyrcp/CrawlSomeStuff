e0f683d5544bac2ac744fb59318b56f103d058ae
atyrcp <atyrcp@gmail.com>
2019-08-18 20:06:27 +0800

    colon in url

--- Crawl Some Stuff/Network.swift
+++ Crawl Some Stuff/Network.swift
@@ -12,15 +12,15 @@ class Network {
     
     
     func getSome(success: @escaping (Data) -> Void) {
-        let endPoint = "https://apistage2.aisleconnect.us/ac.server/oauth/token"
-        let info = "grant_type=password&username=paul.lin@lineagenetworks.com&password=welcome1&client_id=my-client&client_secret=my-secret&scope=read&method=POST"
-        
+        let endPoint = "https://apistage2.aisleconnect.us/ac.server/rest/v2.5/checklist/:id"
+//        let info = "grant_type=password&username=paul.lin@lineagenetworks.com&password=welcome1&client_id=my-client&client_secret=my-secret&scope=read&method=POST"
+        let paramater = ":id"
         let url = URL(string: endPoint)
         var request = URLRequest(url: url!)
-        request.httpMethod = "POST"
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        request.httpBody = info.data(using: String.Encoding.utf8)!
+//        request.httpMethod = "POST"
+//        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+//        request.setValue("application/json", forHTTPHeaderField: "Accept")
+//        request.httpBody = info.data(using: String.Encoding.utf8)!
         
         let session = URLSession.shared
         
